bc9ea782ea33eb1e3fb0334e2c6e8a657bca41be
evan-mick <evsammick@gmail.com>
2024-10-12 15:54:23 -0400

    more data stuff

--- Assets/GameData/Day.cs
+++ Assets/GameData/Day.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Day : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+/*
+ * Make sure some timing thing for the ordered calls?
+ */
+
+[System.Serializable]
+public struct DialogueHolder
+{
+    [SerializeField]
+    public Tag[] requiredTags;
+    public Dialogue dialogue;
+}
+
+[System.Serializable]
+public struct SingleDayDialogueList
+{
+    public DialogueHolder[] dialogue;
+}
+
+// Day thing
+// Each day has array of potential ordered call pools
+// Within those, you specify individual dialogues which have required tags
+// Then, upon something being selected, the first one with the required tags is picked
+[CreateAssetMenu(fileName = "Day", menuName = "GameData/Day", order = 1)]
+public class Day : ScriptableObject
+{
+    public SingleDayDialogueList[] RandomizedCallPool;
+    public SingleDayDialogueList[] OrderedCallPool;
 }
