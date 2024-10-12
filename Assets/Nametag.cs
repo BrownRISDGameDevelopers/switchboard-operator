55ab0b27c73cb44f118e8a3b2070f085e6b553cf
Joshua Ehrlich <21961358+joshuatehrlich@users.noreply.github.com>
2024-10-12 16:58:46 -0400

    create nametag class for book ui

---
+++ Assets/Nametag.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class Nametag : MonoBehaviour
+{
+    Location location;
+    CharacterInfo characterInfo;
+    [SerializeField] public TMP_Text nameText;
+    [SerializeField] public TMP_Text locationText;
+}
