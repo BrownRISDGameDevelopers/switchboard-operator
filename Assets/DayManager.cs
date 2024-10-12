87d3ba392afa718e227b3260694950e19e3bea91
evan-mick <evsammick@gmail.com>
2024-10-12 13:51:17 -0400

    location manager

--- Assets/DayManager.cs
+++ Assets/DayManager.cs
@@ -2,23 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DayManager : MonoBehaviour
 {
 
-    private HashSet<string> tagsReference; 
+    private HashSet<string> tagsReference;
 
-    public void SetTagsReference(HashSet<string> newTags) {
-        tagsReference = newTags; 
+    public void SetTagsReference(HashSet<string> newTags)
+    {
+        tagsReference = newTags;
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Call upon switch board
+        // connect events
+
+    }
+
+    void OnSwitchBoardEvent()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
