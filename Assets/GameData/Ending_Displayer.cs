88a0ebafa6a4c0a85c41a2ce826779e03a3e294a
evan-mick <evsammick@gmail.com>
2024-10-19 15:14:51 -0400

    commented dialogue

--- Assets/GameData/Ending_Displayer.cs
+++ Assets/GameData/Ending_Displayer.cs
@@ -1,28 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class Ending_Displayer : MonoBehaviour
+/*public class Ending_Displayer : MonoBehaviour
 {
-    private string endingText 
-    private Texture2D[] endingPortraits
+    private string endingText;
+    private Sprite[] endingPortraits;
 
     // Reference the UI elements in the Inspector
     [SerializeField] private Text endingTextUI; // Assign via the Inspector or code
-    [SerializeField] private RawImage endingPortraitUI; // Assign via the Inspector or code
+    [SerializeField] private Image endingPortraitUI; // Assign via the Inspector or code
 
     // Start is called before the first frame update
     void Start()
     {
         //Ensure that UI elements are cleared initially
         if (endingTextUI != null) endingTextUI.text = "If this shows we have a problem";
-        if (endingPortraitUI != null) endingPortraitUI.texture = null;
-    } 
+        if (endingPortraitUI != null) endingPortraitUI.sprite = null;
+    }
     //Call this when you have an ending you want to show. Needs something to prevent controls. 
-    public void displayEnding(Ending myEnding) {
-        endingText = myEnding.text  
-        endingPortraits = myEnding.portraits  
-         // Update UI components
+    public void displayEnding(Ending myEnding)
+    {
+        endingText = myEnding.text;
+        endingPortraits = myEnding.portraits;
+        // Update UI components
         if (endingTextUI != null)
         {
             endingTextUI.text = endingText;
@@ -31,20 +31,21 @@ public class Ending_Displayer : MonoBehaviour
         if (endingPortraitUI != null && endingPortraits.Length > 0)
         {
             // For now, I'm going to display the first portrait (you can modify this to cycle through all portraits)
-            endingPortraitUI.texture = endingPortraits[0];
-            endingTextUI.text = endingText; 
-        } 
-        if (endingPortraits.Length <=0) {
+            endingPortraitUI.sprite = endingPortraits[0];
+            endingTextUI.text = endingText;
+        }
+        if (endingPortraits.Length <= 0)
+        {
             //you fucked up if this happened. 
         }
         // Optionally, disable player controls here
         // Something like DisablePlayerControls();
     }
 
-    
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
+}*/
