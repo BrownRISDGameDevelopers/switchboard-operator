7f9021fd6b939f32d8a0bc81549df1ee629a2cf5
evan-mick <evsammick@gmail.com>
2024-11-09 16:50:01 -0500

    ending change start

--- Assets/Scripts/ScriptableObjects/Ending.cs
+++ Assets/Scripts/ScriptableObjects/Ending.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum EndingType
+{
+    EndOfDays = 0,
+    InstantTagsAcquired = 1,
+    EndOfAnyDay = 2,
+}
+
 [CreateAssetMenu(fileName = "Ending", menuName = "GameData/Ending", order = 1)]
 public class Ending : ScriptableObject
 {
     public Tag[] requiredTags;
     public string Text;
     public Texture2D[] portrait;
+    public bool instantEndOnRequirements = false;
 }
