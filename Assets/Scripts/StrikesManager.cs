82bd0509b7911470f643a803baf5e622acc764a3
evan-mick <evsammick@gmail.com>
2024-11-09 15:51:14 -0500

    strike display

--- Assets/Scripts/StrikesManager.cs
+++ Assets/Scripts/StrikesManager.cs
@@ -3,42 +3,51 @@ using System.Collections;
 
 public class StrikeManager : MonoBehaviour
 {
-	public GameObject[] strikeLights;
-	private int strikeCount = 0;
-	private const int maxStrikes = 3;
+   public GameObject[] strikeLights;
+   private int strikeCount = 0;
+   private const int maxStrikes = 3;
 
-	public void RegisterStrike()
-	{
-		strikeCount++;
-		strikeLights[strikeCount - 1].SetActive(true);
+   void Start()
+   {
+	  ResetStrikes();
+	  DayManager.onStrike += OnStrike;
+	  // print("Starting");
+	  // StartCoroutine(ResetStrikesAfterDelay());
+	  // print("YAY");
+   }
 
-		if (strikeCount >= maxStrikes)
-		{
-			// do something to end the game
-		}
-	}
+   private void OnStrike(int strikesLeft, bool recharge)
+   {
+	  for (int i = 0; i < maxStrikes - strikesLeft; i++)
+	  {
+		 strikeLights[i].SetActive(true);
+	  }
+   }
 
-	public void ResetStrikes()
-	{
-		strikeCount = 0;
-		foreach (var light in strikeLights)
-		{
-			light.SetActive(false);
-		}
-	}
+   public void RegisterStrike()
+   {
+	  strikeCount++;
+	  strikeLights[strikeCount - 1].SetActive(true);
 
-	void Start()
-	{
-		ResetStrikes();
-		// print("Starting");
-		// StartCoroutine(ResetStrikesAfterDelay());
-		// print("YAY");
-	}
+	  if (strikeCount >= maxStrikes)
+	  {
+		 // do something to end the game
+	  }
+   }
 
-	private IEnumerator ResetStrikesAfterDelay()
-	{
-		yield return new WaitForSeconds(3);
-		print("Striking");
-		RegisterStrike();
-	}
+   public void ResetStrikes()
+   {
+	  strikeCount = 0;
+	  foreach (var light in strikeLights)
+	  {
+		 light.SetActive(false);
+	  }
+   }
+
+   private IEnumerator ResetStrikesAfterDelay()
+   {
+	  yield return new WaitForSeconds(3);
+	  print("Striking");
+	  RegisterStrike();
+   }
 }
