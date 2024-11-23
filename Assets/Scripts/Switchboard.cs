02ae2f5958e123e33d5fd3d81a40e1db038f0ab6
evan-mick <evsammick@gmail.com>
2024-11-23 14:17:13 -0500

    little commit

--- Assets/Scripts/Switchboard.cs
+++ Assets/Scripts/Switchboard.cs
@@ -65,7 +65,7 @@ public class Switchboard : MonoBehaviour
 
             jackSwitches[i] = go_switch.GetComponent<Switch>();
             jackSwitches[i].gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
-            
+
             jacks[i] = go_jack.GetComponentInChildren<Jack>();
 
             jackSwitches[i].locationData = new Location()
