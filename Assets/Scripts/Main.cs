01465715cd8c0b4ffc12644a7f47d812bbd39200
AlexanderThaep <odomsathshia_tep@brown.edu>
2024-11-23 15:20:58 -0500

    fat commit

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    public GameObject volumeManagerObject;
+
     void Start()
     {
+        GameObject vm = Instantiate(volumeManagerObject);
+        DontDestroyOnLoad(vm);
+
         SceneManager.LoadScene((int) Constants.SceneIndexTable.Menu);
     }
 }
